e6ab4ef2be8cd5e1889807f6b1310aef59a7790a
Tgnguyen0 <141852665+Tgnguyen0@users.noreply.github.com>
2024-04-17 23:33:04 +0700

    Add files via upload

---
+++ js/ListProduct.js
@@ -0,0 +1,29 @@
+// Danh sách sản phẩm trong trang "Sản phẩm"
+let noichien1 = { ten: "Kem-chong-nang-da-tang-guo-2024", anh: "/img/SanPhamSale/Sua-rua-mat-than-tre-tram-tra-guo.webp", giacu: "500.000₫", giamoi: "399.000" };
+let noichien2 = { ten: "Kem-chong-nang-da-tang-guo-20247", anh: "/img/SanPham/SRuaMat/1a90e792bfcef500b919f3feedfcdfdcc637e8f68ffc47de55a0fc59b9a9a3a3.jpeg", giacu: "310.000₫", giamoi: "250.000" };
+let noichien3 = { ten: "Kem-chong-nang-da-tang-guo-2024", anh: "/img/SanPham/SRuaMat/8935006543140-front_9ebdb35bbbf94be8bbbbe3686139e7d0_c3salmgszwa4da3d.jpg", giacu: "890.000₫", giamoi: "700.000" };
+let noichien4 = { ten: "Kem-chong-nang-da-tang-guo-2024", anh: "/img/SanPham/SRuaMat/e366994e39bd8aae256e06ac336f42051a17d3d6512b8bd60e6bb8e639734b84.jpeg", giacu: "200.000₫", giamoi: "90.000" };
+let noichien5 = { ten: "Kem-chong-nang-da-tang-guo-2024", anh: "/img/SanPham/SRuaMat/f63a8285bc5a6767c16c4b31e25c944d6375da2f9a604299036505ebc5c7e34e.jpeg", giacu: "90.000₫", giamoi: "50.000" };
+let noichien6 = { ten: "Kem-chong-nang-da-tang-guo-2024", anh: "/img/SanPham/SRuaMat/3021183_1.jpg", giacu: "330.000₫", giamoi: "216.000" };
+
+dsKemChongNang = [noichien1, noichien2, noichien3, noichien4, noichien5, noichien6];
+
+
+// Danh sách sản phẩm giảm giá
+let sale1 = { ten: "Bộ Mỹ Phẩm Dưỡng Môi Mềm Mại, Rạng Rỡ", anh: "/img/SanPhamSale/Set-qua-tang-an-lanh-2-guo-768x768.webp", giacu: "420.000₫", giamoi: "395.000₫", phantram: "giảm 6%" }
+let sale2 = { ten: "Sữa Rửa Mặt Than Tre Tràm Trà GUO", anh: "/img/SanPhamSale/Set-qua-tang-tinh-khoi-1-guo.webp", giacu: "1.350.000₫", giamoi: "890.000₫", phantram: "Giảm 34%" }
+let sale3 = { ten: "Nước Tẩy Trang Đa Năng B5 GUO", anh: "/img/SanPhamSale/Nuoc-tay-trang-b5-guo.webp", giacu: "170.000₫", giamoi: "109.000₫", phantram: "Giảm 35%" }
+let sale4 = { ten: "Kem Chống Nắng Đa Năng GUO 2 trong 1", anh: "/img/SanPhamSale/Kem-chong-nang-da-tang-guo-2024.webp", giacu: "1.650.000₫", giamoi: "990.000₫", phantram: "Giảm 40%" }
+let sale5 = { ten: "Dầu Tẩy Trắng Tràm Trà Gừng GUO", anh: "/img/SanPhamSale/Dau-tay-trang-tram-tra-guo-vo-moi.webp", giacu: "2.260.000₫", giamoi: "1.590.000₫", phantram: "Giảm 29%" }
+
+dsSale = [sale1, sale2, sale3, sale4, sale5];
+
+// Danh sách sản phẩm trong trang "Sản phẩm"
+let sd1 = { ten: "Kem-chong-nang-da-tang-guo-2024", anh: "", giacu: "500.000₫", giamoi: "399.000" };
+let sd2 = { ten: "Kem-chong-nang-da-tang-guo-20247", anh: "/img/SanPham/Son duong/1.jpeg", giacu: "310.000₫", giamoi: "250.000" };
+let sd3 = { ten: "Kem-chong-nang-da-tang-guo-2024", anh: "/img/SanPham/Son duong/1.jpeg", giacu: "890.000₫", giamoi: "700.000" };
+let sd4 = { ten: "Kem-chong-nang-da-tang-guo-2024", anh: "/img/SanPham/Son duong/1.jpeg", giacu: "200.000₫", giamoi: "90.000" };
+let sd5 = { ten: "Kem-chong-nang-da-tang-guo-2024", anh: "/img/SanPham/Son duong/1.jpeg", giacu: "90.000₫", giamoi: "50.000" };
+let sd6 = { ten: "Kem-chong-nang-da-tang-guo-2024", anh: "/img/SanPham/Son duong/1.jpeg", giacu: "330.000₫", giamoi: "216.000" };
+
+dssonduong = [sd1, sd2, sd3, sd4, sd5, sd6];
